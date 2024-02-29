77f20354e733fec997bcb3acc2ad915930332fc0
Salman BuAli <salmanbuali@gmail.com>
2024-02-29 21:28:31 +0300

    creating models and logging in works

--- models/User.js
+++ models/User.js
@@ -4,7 +4,13 @@ const userSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
-    passwordDigest: { type: String, required: true }
+    passwordDigest: { type: String, required: true },
+    type: {type: String, required: true},
+    avatar: {type: String},
+    contact: {type: String},
+    address: {type: String},
+    orders: [{ type: Schema.Types.ObjectId, ref: "Order" }],
+    restId: { type: Schema.Types.ObjectId, ref: "Rest" }
   },
   { timestamps: true }
 )
--- models/index.js
+++ models/index.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose')
 const userSchema = require('./User')
+const orderSchema = require('./Order')
+const restSchema = require('./Restaurant')
+const menuSchema = require('./Menu')
 
 const User = mongoose.model('User', userSchema)
-
+const Order = mongoose.model('Order', orderSchema)
+const Rest = mongoose.model('Rest', restSchema)
+const Menu = mongoose.model('Menu', menuSchema)
 
 module.exports = {
-  User
+  User,
+  Order,
+  Rest,
+  Menu
 }
