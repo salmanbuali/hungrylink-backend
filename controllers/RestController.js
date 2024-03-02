3905526f9458ca1ec6a5e3817df8c1f4b18cb9b0
Salman BuAli <salmanbuali@gmail.com>
2024-03-02 13:30:31 +0300

    getting details and fixing bugs

--- controllers/RestController.js
+++ controllers/RestController.js
@@ -1,15 +1,17 @@
 const { Rest } = require('../models')
 const { Menu } = require('../models')
 const { User } = require('../models')
+const { Item } = require('../models')
+const { Category } = require('../models')
 
 const createMenu = async (req, res) => {
-  const user = await User.findById(req.body.user._id)
-  console.log(user)
-  if(user.type === "restaurant"){
+  console.log(req)
+  const user = await User.findById(req.body._id)
+
+  if (user.type === 'restaurant') {
     let restaurant = await Rest.findById(user.restId)
-    console.log(restaurant)
     const newMenu = await Menu.create({
-       restaurant: restaurant._id
+      restaurant: restaurant._id
     })
     restaurant = await Rest.findOneAndUpdate(
       { _id: restaurant._id },
@@ -17,9 +19,57 @@ const createMenu = async (req, res) => {
     )
     res.send(restaurant)
   }
-
 }
 
+const createCategory = async (req, res) => {
+  const user = await User.findById(req.body._id)
+  const restaurant = await Rest.findById(user.restId)
+  const menu = await Menu.findById(restaurant.menu)
+  const newCategory = await Category.create({
+    name: req.body.categoryName
+  })
+  const updatedmenu = await Menu.updateOne(
+    { _id: menu._id },
+    { $push: { categoryId: newCategory } }
+  )
+
+  res.send(newCategory)
+}
+
+const createItem = async (req, res) => {
+  const newItem = await Item.create({
+    name: req.body.itemName,
+    price: req.body.price,
+    qty: req.body.qty,
+    desc: req.body.desc.split(","),
+    pic: req.body.itemPic
+  })
+  await Category.updateOne(
+    {_id: req.body.categoryId},
+    { $push: { items: newItem._id } }
+  )
+  res.send(newItem)
+}
+
+const getRestDetails = async (req, res) => {
+  const restDetails = await Rest.findById(req.params.restId)
+  const userRest = await User.findOne({restId: req.params.restId})
+  await restDetails.populate('menu')
+  if(restDetails.menu.categoryId != []) {for(let i = 0; i < restDetails.menu.categoryId.length; i++) {
+    await restDetails.menu.categoryId.populate(categoryId[i])
+  }}
+  const response = {
+    restDetails,
+    userRest
+  }
+
+  res.send(response)
+}
+
+
 module.exports = {
-  createMenu
+  createMenu,
+  createCategory,
+  createItem,
+  getRestDetails
 }
