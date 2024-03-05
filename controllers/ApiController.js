ef4e4029c842bbce60f64c4c0b593899dfb134f5
Salman BuAli <salmanbuali@gmail.com>
2024-03-05 17:38:32 +0300

    api working with filtering

--- controllers/ApiController.js
+++ controllers/ApiController.js
@@ -1,29 +1,27 @@
 const { User } = require('../models')
 
-const getRestsByLocation = async ( req, res ) => {
-
-  try { 
+const getRestsByLocation = async (req, res) => {
+  
+  try {
     const rests = await User.find({
       restId: { $ne: null },
-      address: req.body.location.toLowerCase() 
-    });
-  
-    const filteredRests = rests.map(rest => {
-      const { passwordDigest, ...filteredRest } = rest; // Destructure and exclude passwordDigest
-      console.log(filteredRest)
-      return filteredRest;
-    });
+      address: req.body.location.toLowerCase()
+    }).lean()
+    
+    const filteredRests = rests.map((rest) => {
+      const { passwordDigest, orders, type, _id, updatedAt, createdAt, __v, ...filteredRest } = rest 
+      const website = `https://hungrylink.surge.sh/menu/${rest.restId}`
+      return { ...filteredRest, website };
+    })
 
-    if(filteredRests.length <= 0) {
+    if (filteredRests.length <= 0) {
       res.send(`No restaurants found nearby ${req.body.location}`)
     } else {
       res.send(filteredRests)
     }
-    
   } catch (error) {
     res.send(`no restaurants found for ${req.body.location}`)
   }
-  
 }
 
 module.exports = {
